a96ca338c9a9669bf4d748a845bbe6099e4f47fb
Park Hyunwoo <ez@smartstudy.co.kr>
2017-11-21 13:56:18 +0900

    Dropping support under Django 1.7

--- django_summernote/widgets.py
+++ django_summernote/widgets.py
@@ -11,13 +11,7 @@ from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
 from django.utils.translation import get_language
-
-try:
-    # Django >= 1.7
-    from django.forms.utils import flatatt
-except ImportError:
-    # Django < 1.7
-    from django.forms.util import flatatt
+from django.forms.utils import flatatt
 from django_summernote.settings import summernote_config
 from django.conf import settings
 
