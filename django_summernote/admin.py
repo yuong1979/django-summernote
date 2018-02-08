f61e84c5404907cc49e681f33387f7155263149e
Park Hyunwoo <ez.amiryo@gmail.com>
2018-02-08 17:03:01 +0900

    Rename summer_note to summernote

--- django_summernote/admin.py
+++ django_summernote/admin.py
@@ -8,27 +8,27 @@ __widget__ = SummernoteWidget if summernote_config['iframe'] \
 
 
 class SummernoteInlineModelAdmin(admin.options.InlineModelAdmin):
-    summer_note_fields = '__all__'
+    summernote_fields = '__all__'
 
     def formfield_for_dbfield(self, db_field, **kwargs):
-        if self.summer_note_fields == '__all__':
+        if self.summernote_fields == '__all__':
             if isinstance(db_field, models.TextField):
                 kwargs['widget'] = __widget__
         else:
-            if db_field.name in self.summer_note_fields:
+            if db_field.name in self.summernote_fields:
                 kwargs['widget'] = __widget__
         return super(SummernoteInlineModelAdmin, self).formfield_for_dbfield(db_field, **kwargs)
 
 
 class SummernoteModelAdmin(admin.ModelAdmin):
-    summer_note_fields = '__all__'
+    summernote_fields = '__all__'
 
     def formfield_for_dbfield(self, db_field, **kwargs):
-        if self.summer_note_fields == '__all__':
+        if self.summernote_fields == '__all__':
             if isinstance(db_field, models.TextField):
                 kwargs['widget'] = __widget__
         else:
-            if db_field.name in self.summer_note_fields:
+            if db_field.name in self.summernote_fields:
                 kwargs['widget'] = __widget__
         return super(SummernoteModelAdmin, self).formfield_for_dbfield(db_field, **kwargs)
 
