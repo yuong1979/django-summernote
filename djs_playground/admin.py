053832182ae22dd4435efce027397f436291c9c1
Park Hyunwoo <ez.amiryo@gmail.com>
2018-02-22 15:10:32 +0900

    Add an example for inline model admin

--- djs_playground/admin.py
+++ djs_playground/admin.py
@@ -2,11 +2,21 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from django_summernote.admin import SummernoteModelAdmin
-from .models import Post
+from django_summernote.admin import SummernoteModelAdmin, SummernoteModelAdminMixin
+from .models import Post, Book, Author
 
 
 class PostAdmin(SummernoteModelAdmin):
     pass
 
 admin.site.register(Post, PostAdmin)
+
+class BookInline(SummernoteModelAdminMixin, admin.StackedInline):
+    model = Book
+
+class AuthorAdmin(SummernoteModelAdminMixin, admin.ModelAdmin):
+    model = Author
+    inlines = [
+        BookInline,
+    ]
+admin.site.register(Author, AuthorAdmin)
--- djs_playground/models.py
+++ djs_playground/models.py
@@ -3,7 +3,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Sample model for testing admin page
+
 class Post(models.Model):
     title = models.CharField(max_length=255, help_text="Title of blog posting")
     body = models.TextField(blank=True, null=True)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.TextField(blank=True, null=True)
+
+
+class Book(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    desc = models.TextField(blank=True, null=True)
